b8640a87e7f5c0fb4025ecd58e02aa2bcf03e1b6
Egor20142 <egor.tropin20100526@gmail.com>
2022-05-22 16:30:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+public class Main {
+
+     public static void main(String[] args)
+     {
+          System.out.println("Hello world");
+          System.out.println("Orange");
+          System.out.println("Banana");
+          System.out.println("Apple");
+
+          int age = 0;
+          int a = 10;
+          int b = 15;
+          age = a+b;
+          System.out.println(age);
+
+          int volue = 55;
+
+          if (volue >= 100) {
+               System.out.println("green");
+          }
+          if (volue > 47 && volue < 88) {
+               System.out.println("yellow");
+          }
+          if (volue <= 35) {
+               System.out.println("red");
+          }
+
+
+
+          int A = 15;
+          int B = 10;
+
+          if (A >= B) {
+               System.out.println("A>=B");
+          } else if (A < B) {
+               System.out.println("A<B");
+          } 
+
+
+
+
+
+
+
+
+
+
+
+     }
+
+
+
+
+
+     }
